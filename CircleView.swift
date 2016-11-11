ea02ecf7fb90d7fb0b39c930920994aef52078f0
mogalia786 <imac@Heros-iMac.local>
2016-11-11 14:33:13 +0200

    Created Post Feed UI

--- CircleView.swift
+++ CircleView.swift
@@ -1,24 +1,28 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  CricketInsta
 //
-//  Created by ebrahim on 11/10/16.
+//  Created by ebrahim on 11/11/16.
 //  Copyright © 2016 iSTROBE. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-    
+class CircleView: UIImageView {
+
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor=UIColor.black.cgColor
         layer.shadowOffset=CGSize(width: 1.0, height: 1.0)
         layer.shadowRadius=0.5
-        layer.cornerRadius=2.0
-        
         
     }
     
-    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius=self.frame.height/2
+
+        
+    }
+
 }
--- postVC.swift
+++ postVC.swift
@@ -10,11 +10,13 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class postVC: UIViewController {
+class postVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        TableView.delegate=self
+        TableView.dataSource=self
         // Do any additional setup after loading the view.
     }
 
@@ -23,6 +25,17 @@ class postVC: UIViewController {
         let _=KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier: "signOUT", sender: self)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return cell
+        
     }
    
    }
